d7f18d930b2684e0342ff1fc5b9485487a05d98a
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-06-06 05:06:57 +0900

    コントローラー記述

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,41 @@
 class GamesController < ApplicationController
   before_action :login_check
   before_action :current_user
+  before_action :set_player
+
+  def route    
+    render @player.phase
+  end
+
 
   def start
-    @player = Player.find(@current_user.id)
-    if @player.start == "true"
-      redirect_to games_path
-    end
   end
 
   def beginning
-    @player = Player.find(@current_user.id)
-    @player.update(player_params.merge(start: params[:start]))    
-    redirect_to games_path
+    @player.update(player_params.merge(phase: params[:phase]))    
+    render @player.phase
   end
 
   def index
   end
 
+  def adventure
+    map_1_create
+  end
+
 private
 
   def player_params
-    params.require(:player).permit(:life, :strength, :skill, :exp, :erapse, :cleared, :user_id, :start)
+    params.require(:player).permit(:life, :strength, :skill, :exp, :erapse, :phase, :flag_item, :growth_type, :user_id)
   end
 
   def login_check
     unless logged_in?
       return redirect_to root_path
     end
+  end
+
+  def set_player
+    @player = Player.find(current_user.id)
   end
 end
